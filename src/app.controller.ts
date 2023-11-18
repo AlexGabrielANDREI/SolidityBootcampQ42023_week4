414cb5204ab265c21fedb8648f3bd54dee851a2f
Gabriel <andrei.gaby90@yahoo.com>
2023-11-18 12:59:34 +0200

    initial backend commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MintTokenDto } from './dtos/mintToken.dto';
+import { SubscribersDto } from './dtos/subscribers.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,55 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('something-else')
+  getSomethingElse(): string {
+    return this.appService.getSomethingElse();
+  }
+
+  @Get('contract-address')
+  getContractAddress() {
+    return { result: this.appService.getContractAddress() };
+  }
+  @Get('token-name')
+  async getTokenName() {
+    return { result: await this.appService.getTokenName() };
+  }
+  @Get('total-supply')
+  async getTotalSupply() {
+    return { result: await this.appService.getTotalSupply() };
+  }
+
+  @Get('token-balance/:address')
+  async getTokenBalance(@Param('address') address: string) {
+    return { result: await this.appService.getTokenBalance(address) };
+  }
+
+  @Get('transaction-receipt')
+  async getTransactionReceipt(@Query('hash') hash: string) {
+    return { result: await this.appService.getTransactionReceipt(hash) };
+  }
+  //
+  @Get('server-wallet-address')
+  getServerWalletAddress() {
+    return { result: this.appService.getServerWalletAddress() };
+  }
+
+  @Get('check-minter-role')
+  async checkMinterRole(@Query('address') address: string) {
+    return { result: await this.appService.checkMinterRole(address) };
+  }
+
+  @Post('mint-tokens')
+  async mintTokens(@Body() body: MintTokenDto) {
+    const { address, value } = body;
+    return { result: await this.appService.mintTokens(address, value) };
+  }
+
+  @Post('granted-role')
+  async givingRole(@Body() body: SubscribersDto) {
+    const { address } = body;
+    return { result: await this.appService.givingRole(address) };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,86 @@
 import { Injectable } from '@nestjs/common';
+import { ethers } from 'ethers';
+import * as tokenJson from './assets/MyToken.json';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  contract: ethers.Contract;
+  provider: ethers.Provider;
+  wallet: ethers.Wallet;
+
+  constructor(private configService: ConfigService) {
+    this.provider = new ethers.JsonRpcProvider(
+      this.configService.get<string>('RPC_ENDPOINT_URL'),
+    );
+    this.wallet = new ethers.Wallet(
+      this.configService.get<string>('PRIVATE_KEY'),
+      this.provider,
+    );
+    this.contract = new ethers.Contract(
+      this.configService.get<string>('TOKEN_ADDRESS'),
+      tokenJson.abi,
+      this.wallet,
+    );
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello ljjjd!';
+  }
+
+  getSomethingElse(): string {
+    return 'Something else';
+  }
+
+  getContractAddress(): string {
+    return this.configService.get<string>('TOKEN_ADDRESS');
+  }
+
+  async getTokenName(): Promise<string> {
+    const name = await this.contract.name();
+    return name;
+  }
+
+  async getTotalSupply() {
+    const totalSupply = await this.contract.totalSupply();
+    return ethers.formatUnits(totalSupply);
+  }
+  async getTokenBalance(address: string) {
+    const balance = await this.contract.balanceOf(address);
+    return ethers.formatUnits(balance);
+  }
+  async getTransactionReceipt(hash: string) {
+    const txReceipt = await this.provider.getTransaction(hash);
+    return txReceipt;
+  }
+
+  getServerWalletAddress() {
+    return this.wallet.address;
+  }
+  async checkMinterRole(address: string) {
+    const MINTER_ROLE = await this.contract.MINTER_ROLE(); //geting the address of the deployed SC FROM .ENV
+    const hasRole = await this.contract.hasRole(MINTER_ROLE, address);
+    return hasRole;
+  }
+
+  async givingRole(address: string) {
+    try {
+      const code = await this.contract.MINTER_ROLE();
+      const roleTx = await this.contract.grantRole(code, address);
+      await roleTx.wait(); // Wait for the transaction to be mined
+      return { success: true, transactionHash: roleTx.hash };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
+  }
+
+  async mintTokens(address: string, value: string) {
+    try {
+      const tx = await this.contract.mint(address, ethers.parseUnits(value));
+      await tx.wait(); // Wait for the transaction to be mined
+      return { success: true, transactionHash: tx.hash };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
   }
 }
